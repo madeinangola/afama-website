4bcf7546a90a4fc05b954a7e1ac551d9974e988e
eladioclaudio <elladioclaudio@gmail.com>
2022-01-17 18:12:06 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@
 import './styles/styles.scss';
 
 
+document.addEventListener('click', (event) => {
+    let element = event.target;
+
+    if(element.classList.contains('fa-github')) {
+        window.open('https://github.com/madeinangola/afama', '_blank');
+    }
+})
